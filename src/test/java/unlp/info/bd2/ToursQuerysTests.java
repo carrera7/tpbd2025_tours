4f253b664551f2cf2617cfa1ec7aa20477fdb342
carrera7 <josuecarrera788@gmail.com>
2025-04-10 20:54:25 -0300

    funcion de testquerys

--- src/test/java/unlp/info/bd2/ToursQuerysTests.java
+++ src/test/java/unlp/info/bd2/ToursQuerysTests.java
@@ -16,10 +16,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
 
+import junit.framework.Assert;
 import unlp.info.bd2.config.AppConfig;
 import unlp.info.bd2.config.HibernateConfiguration;
 import unlp.info.bd2.model.Route;
+import unlp.info.bd2.model.Service;
 import unlp.info.bd2.model.Stop;
+import unlp.info.bd2.model.TourGuideUser;
 import unlp.info.bd2.services.ToursService;
 import unlp.info.bd2.utils.DBInitializer;
 import unlp.info.bd2.utils.ToursException;
@@ -78,51 +81,55 @@ public class ToursQuerysTests {
     }
 
 
-//     @Test
-//     void getRoutsNotSellTest() throws ToursException {
-//         List<Route> routsNotSell = this.service.getRoutsNotSell();
-//         assertEquals(1, routsNotSell.size());
-//         this.assertListEquality(routsNotSell.stream().map(Route::getName).collect(Collectors.toList()), List.of("Ruta vacia"));
-//     }
+    @Test
+    void getRoutsNotSellTest() throws ToursException {
+        List<Route> routsNotSell = this.service.getRoutsNotSell();
+        assertEquals(1, routsNotSell.size());
+        // Transforma la lista de rutas en una lista de nombres de rutas
+        // y compara con la lista esperada
+        this.assertListEquality(routsNotSell.stream().map(Route::getName).collect(Collectors.toList()), List.of("Ruta vacia"));
+    }
 
-//     @Test
-//     void getTop3RoutesWithMaxRatingTest() throws ToursException {
-//         List<Route> routesWithMaxRating = this.service.getTop3RoutesWithMaxRating();
-//         assertEquals(3, routesWithMaxRating.size());
-//         this.assertListEquality(routesWithMaxRating.stream().map(Route::getName).collect(Collectors.toList()), List.of("City Tour", "Historical Adventure", "Architectural Expedition"));
-//     }
+    @Test
+    void getTop3RoutesWithMaxRatingTest() throws ToursException {
+        List<Route> routesWithMaxRating = this.service.getTop3RoutesWithMaxRating();
+        assertEquals(3, routesWithMaxRating.size());
+        // Transforma la lista de rutas en una lista de nombres de rutas
+        // y compara con la lista esperada
+        this.assertListEquality(routesWithMaxRating.stream().map(Route::getName).collect(Collectors.toList()), List.of("City Tour", "Historical Adventure", "Architectural Expedition"));
+    }
 
-//     @Test
-//     void getMostDemandedServiceTest() throws ToursException {
-//         Service mostDemandedService = this.service.getMostDemandedService();
-//         assertEquals("souvenir t-shirt", mostDemandedService.getName());
-//         assertEquals("I love Buenos Aires t-shirt", mostDemandedService.getDescription());
-//     }
+    // @Test
+    // void getMostDemandedServiceTest() throws ToursException {
+    //     Service mostDemandedService = this.service.getMostDemandedService();
+    //     assertEquals("souvenir t-shirt", mostDemandedService.getName());
+    //     assertEquals("I love Buenos Aires t-shirt", mostDemandedService.getDescription());
+    // }
 
-//     @Test
-//     void getServiceNoAddedToPurchasesTest() throws ToursException {
-//         List<Service> serviceNoAddedToPurchases = this.service.getServiceNoAddedToPurchases();
-//         assertEquals(2, serviceNoAddedToPurchases.size());
-//         this.assertListEquality(serviceNoAddedToPurchases.stream().map(Service::getName).collect(Collectors.toList()), List.of("Architectural Expedition Book", "souvenir retrato"));
-//     }
+    @Test
+    void getServiceNoAddedToPurchasesTest() throws ToursException {
+        List<Service> serviceNoAddedToPurchases = this.service.getServiceNoAddedToPurchases();
+        assertEquals(2, serviceNoAddedToPurchases.size());
+        this.assertListEquality(serviceNoAddedToPurchases.stream().map(Service::getName).collect(Collectors.toList()), List.of("Architectural Expedition Book", "souvenir retrato"));
+    }
 
-//     @Test
-//     void getTourGuidesWithRating1Test() throws ToursException {
-//         List<TourGuideUser> tourGuidesWithRating1 = this.service.getTourGuidesWithRating1();
-//         assertEquals(3, tourGuidesWithRating1.size());
-//         this.assertListEquality(tourGuidesWithRating1.stream().map(TourGuideUser::getUsername).collect(Collectors.toList()), List.of("userG1", "userG3", "userG4"));
-//     }
+    @Test
+    void getTourGuidesWithRating1Test() throws ToursException {
+        List<TourGuideUser> tourGuidesWithRating1 = this.service.getTourGuidesWithRating1();
+        assertEquals(3, tourGuidesWithRating1.size());
+        this.assertListEquality(tourGuidesWithRating1.stream().map(TourGuideUser::getUsername).collect(Collectors.toList()), List.of("userG1", "userG3", "userG4"));
+    }
 
-//     private <T> void assertListEquality(List<T> list1, List<T> list2) {
-//         if (list1.size() != list2.size()) {
-//             Assert.fail("Lists have different size");
-//         }
+    private <T> void assertListEquality(List<T> list1, List<T> list2) {
+        if (list1.size() != list2.size()) {
+            Assert.fail("Lists have different size");
+        }
 
-//         for (T objectInList1 : list1) {
-//             if (!list2.contains(objectInList1)) {
-//                 Assert.fail(objectInList1 + " is not present in list2");
-//             }
-//         }
-//     }
+        for (T objectInList1 : list1) {
+            if (!list2.contains(objectInList1)) {
+                Assert.fail(objectInList1 + " is not present in list2");
+            }
+        }
+    }
 
 }
